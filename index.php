fe9fb840b4d1f78e7ad4c7944a96240005f08101
espositogaetano87@gmail.com <espositogaetano87@gmail.com@d1296418-1913-72db-0a64-2c57c6bf778b>
2011-07-19 19:39:36 +0000

    bozza iniziale sito completata

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php
-echo "Punto Casa website";
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Home Page | Punto Casa Tendaggi di Umbero Speranza</title>
+  <meta name="description" content="Home page del sito Punto Casa Tendaggi di Umberto Speranza" />
+<?php   require_once 'init.php'; ?>
+
+<?php   require_once 'finish.php'; ?>
