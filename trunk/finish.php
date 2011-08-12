b2d4ff59f4e98e427396ab86115318fd8224197f
espositogaetano87@gmail.com <espositogaetano87@gmail.com@d1296418-1913-72db-0a64-2c57c6bf778b>
2011-08-12 13:19:20 +0000

    bugfix

--- trunk/finish.php
+++ trunk/finish.php
@@ -10,71 +10,73 @@
                     <a href="sitemap.php">Mappa Sito</a>
                     <span style="float:right;">
                       Design &amp; Developement:
-                      <a href="#" class="contatti_personali" id="mario_speranza"> Mario Speranza</a> -
-                      <a href="#" class="contatti_personali" id="gaetano_esposito">Gaetano Esposito</a>
+                      <a href="finish.php#" class="contatti_personali" id="mario_speranza"> Mario Speranza</a> -
+                      <a href="finish.php#" class="contatti_personali" id="gaetano_esposito">Gaetano Esposito</a>
                     </span>
                     <div style="text-align:right;">&copy; 2011 Tutti i diritti sono riservati </div>
                 </div>
             </div>
-            <div id="mario_speranza_contatti" class="formError" style="top:555px; left:623px; display:none">
-              <div class="formErrorContent link_contatti_personali">
+            <div id="mario_speranza_contatti" class="boxContatti" style="top:555px; left:623px; display:none">
+              <div class="boxContattiContent link_contatti_personali">
                 <div style="text-align:center;">I miei contatti:</div>
                 <table style="border:none">
                   <tr>
-                    <td><a href="mailto:mariosperanza88@gmail.com" onclick="targetBlank(this.href);return false;">E-mail</a></td>
+                    <td><a class="email_personale" href="mailto:mariosperanza88@gmail.com">E-mail</a></td>
                     <td><a href="http://it.linkedin.com/pub/mario-speranza/25/a10/253" onclick="targetBlank(this.href);return false;">LinkedIn</a> <br /></td>
                   </tr>
                   <tr>
                     <td style="padding-right:55px"><a href="http://www.facebook.com/mario.speranza" onclick="targetBlank(this.href);return false;">Facebook</a></td>
-                    <td><a href="http://twitter.com/#!/mr_hope88" onclick="targetBlank(this.href);return false;">Twitter</a></td>
+                    <td><a href="http://twitter.com/finish.php#!/mr_hope88" onclick="targetBlank(this.href);return false;">Twitter</a></td>
                   </tr>
                 </table>
               </div>
-              <div class="formErrorArrow" style="margin: -2px 0 0 80px;">
-                <div class="line10"></div>
-                <div class="line9"></div>
-                <div class="line8"></div>
-                <div class="line7"></div>
-                <div class="line6"></div>
-                <div class="line5"></div>
-                <div class="line4"></div>
-                <div class="line3"></div>
-                <div class="line2"></div>
-                <div class="line1"></div>
+              <div class="boxContattiArrow" style="margin: -2px 0 0 80px;">
+                <div class="linea10"></div>
+                <div class="linea9"></div>
+                <div class="linea8"></div>
+                <div class="linea7"></div>
+                <div class="linea6"></div>
+                <div class="linea5"></div>
+                <div class="linea4"></div>
+                <div class="linea3"></div>
+                <div class="linea2"></div>
+                <div class="linea1"></div>
               </div>
             </div>
-            <div id="gaetano_esposito_contatti" class="formError" style="top:555px; left:730px; display:none;">
-              <div class="formErrorContent link_contatti_personali">
+            <div id="gaetano_esposito_contatti" class="boxContatti" style="top:555px; left:730px; display:none;">
+              <div class="boxContattiContent link_contatti_personali">
                 <div style="text-align:center;">I miei contatti:</div>
                 <table style="border:none">
                   <tr>
-                    <td><a href="mailto:espositogaetano@gmail.com" onclick="targetBlank(this.href);return false;">E-mail</a></td>
+                    <td><a class="email_personale" href="mailto:espositogaetano87@gmail.com">E-mail</a></td>
                     <td><a href="http://www.linkedin.com/pub/gaetano-esposito/24/8b/636" onclick="targetBlank(this.href);return false;">LinkedIn</a> <br /></td>
                   </tr>
                   <tr>
                     <td style="padding-right:55px"><a href="http://www.facebook.com/gaetano.esposito87" onclick="targetBlank(this.href);return false;">Facebook</a></td>
-                    <td><a href="http://twitter.com/#!/tanux87" onclick="targetBlank(this.href);return false;">Twitter</a></td>
+                    <td><a href="http://twitter.com/finish.php#!/tanux87" onclick="targetBlank(this.href);return false;">Twitter</a></td>
                   </tr>
                 </table>
               </div>
-              <div class="formErrorArrow" style="margin: -2px 0 0 80px;">
-                <div class="line10"></div>
-                <div class="line9"></div>
-                <div class="line8"></div>
-                <div class="line7"></div>
-                <div class="line6"></div>
-                <div class="line5"></div>
-                <div class="line4"></div>
-                <div class="line3"></div>
-                <div class="line2"></div>
-                <div class="line1"></div>
+              <div class="boxContattiArrow" style="margin: -2px 0 0 80px;">
+                <div class="linea10"></div>
+                <div class="linea9"></div>
+                <div class="linea8"></div>
+                <div class="linea7"></div>
+                <div class="linea6"></div>
+                <div class="linea5"></div>
+                <div class="linea4"></div>
+                <div class="linea3"></div>
+                <div class="linea2"></div>
+                <div class="linea1"></div>
               </div>
             </div>
         </div> <!-- chiude content_all-->
         <script type="text/javascript">
+          var email;
           $('.contatti_personali').mouseover(function(){
             id=$(this).attr('id');
             if (id == 'mario_speranza'){
+              email = 'mariosperanza88@gmail.com';
               $('#gaetano_esposito_contatti').fadeOut();
               mario = $('#'+id+'_contatti');
               mario.fadeIn().click(function(){
@@ -83,12 +85,16 @@
             }
             if (id == 'gaetano_esposito'){
               $('#mario_speranza_contatti').fadeOut();
+              email = 'espositogaetano87@gmail.com';
               gaetano = $('#'+id+'_contatti');
               gaetano.fadeIn().click(function(){
                 $(this).fadeOut();
               });
             }
           });
+          $('.email_personale').click(function(){
+            alert(email);
+          })
         </script>
     </body>
 </html>
